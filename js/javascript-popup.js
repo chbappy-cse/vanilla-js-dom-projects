e8ccf9655d8c708d7889fff5413370764de3c603
Ch <chbappy.cse@gmail.com>
2025-09-23 14:24:42 +0600

    JavaScript Custom Popup Modal

---
+++ js/javascript-popup.js
@@ -0,0 +1,8 @@
+const chPopup = document.getElementById('chPopup');
+
+function openPopup() {
+    chPopup.classList.add('ch-open-popup');
+}
+function closePopup() {
+    chPopup.classList.remove('ch-open-popup');
+}
